53a5f2b87e12cb1d52ac0ed6a928e76f922f0b61
Paulo Stocker <paulo.stocker@gesplan.com.br>
2017-03-21 14:52:55 -0300

    1

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('This portal has been closed, you shall not pass!');
+    
+}).listen(process.env.PORT || 8080);
